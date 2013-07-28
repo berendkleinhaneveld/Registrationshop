1156ec9a7ebc5710341fdd6b1293065ba7051153
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2013-07-28 21:54:22 +0200

    Refactor of test cases.
    Replace assert calls to self.assert statements.

--- tests/test_Parameter.py
+++ tests/test_Parameter.py
@@ -6,59 +6,48 @@ class ParameterTest(unittest.TestCase):
 	def testParameter(self):
 		# Test init function
 		param = Parameter("key", 10)
-		assert param.key() is "key"
-		assert param.value() == 10
+		self.assertEquals(param.key(), "key")
+		self.assertEquals(param.value(), 10)
 		# Test string representation
-		assert param.__str__() == "(key 10)"
+		self.assertEquals(param.__str__(), "(key 10)")
 		# Test changing the key
 		param.setKey("otherKey")
-		assert param.key() == "otherKey"
+		self.assertEquals(param.key(), "otherKey")
 
 		# Test simple object creation
 		param = Parameter()
-		assert param.key() == None
-		assert param.value() == None
+		self.assertIsNone(param.key())
+		self.assertIsNone(param.value())
 
 		# Test setting the values with the KeyValue function
 		param.setKeyValue("otherKey", 20)
-		assert param.key() == "otherKey"
-		assert param.value() == 20
+		self.assertEquals(param.key(), "otherKey")
+		self.assertEquals(param.value(), 20)
 
 		# Test with setting only the key
 		param = Parameter("key")
-		assert param.key() == "key"
-		assert param.value() == None
+		self.assertEquals(param.key(), "key")
+		self.assertIsNone(param.value())
 
 		param = Parameter("key", 0.4)
 		otherParam = Parameter(''.join(['k','e','y']), 0.4)
-		assert param == otherParam
+		self.assertEquals(param, otherParam)
 
 		# Strip characters from key, but not from value
 		param.setKey("    Key With Spaces  \n")
-		assert param.key() == "KeyWithSpaces"
+		self.assertEquals(param.key(), "KeyWithSpaces")
 		param.setValue("Value with spaces")
-		assert param.value() == "Value with spaces"
+		self.assertEquals(param.value(), "Value with spaces")
 
 		self.assertFalse(param == Parameter("Hello", 10))
 		self.assertTrue(param != Parameter("Hello", 10))
 		self.assertFalse(param != param)
 
 		# Test setting a value when Key is still None
-		try:
-			# This should raise an exception
-			param = Parameter(None, "value")
-			assert False, "No exception is raised while a parameter was constructed\
-				with a key with value 'None'"
-		except AssertionError, e:
-			raise e
-		except Exception, e:
-			assert isinstance(e, AttributeError)
+		self.assertRaises(AttributeError, Parameter, None, "value")
 
-		try:
-			param = Parameter()
-			param.setValue(20)
-		except Exception, e:
-			assert isinstance(e, AttributeError)
+		param = Parameter()
+		self.assertRaises(AttributeError, param.setValue, 20)
 
 	def testConversionMethods(self):
 		# Create list of elements with different types
@@ -68,81 +57,81 @@ class ParameterTest(unittest.TestCase):
 		for value in values:
 			newValue, success = Parameter.valueAsBool(value)
 			if success:
-				assert type(newValue) == bool
-				assert value == True or value == "true"
+				self.assertIsInstance(newValue, bool)
+				self.assertTrue(value == True or value == "true")
 			newValue, success = Parameter.valueAsInt(value)
 			if success:
-				assert type(newValue) == int
-				assert newValue == 20
+				self.assertIsInstance(newValue, int)
+				self.assertEquals(newValue, 20)
 			newValue, success = Parameter.valueAsFloat(value)
 			if success:
-				assert type(newValue) == float
-				assert newValue == 0.4
+				self.assertIsInstance(newValue, float)
+				self.assertEquals(newValue, 0.4)
 
 		param = Parameter("Key", 0)
-		assert isinstance(param.value(), int)
+		self.assertIsInstance(param.value(), int)
 		param = Parameter("Key", "0")
-		assert isinstance(param.value(), int)
+		self.assertIsInstance(param.value(), int)
 		param = Parameter("Key", 0.0)
-		assert isinstance(param.value(), float)
+		self.assertIsInstance(param.value(), float)
 		param = Parameter("Key", "0.0")
-		assert isinstance(param.value(), float)
+		self.assertIsInstance(param.value(), float)
 		param = Parameter("Key", True)
-		assert isinstance(param.value(), bool)
+		self.assertIsInstance(param.value(), bool)
 		param = Parameter("Key", False)
-		assert isinstance(param.value(), bool)
+		self.assertIsInstance(param.value(), bool)
 		param = Parameter("Key", "True")
-		assert isinstance(param.value(), bool)
+		self.assertIsInstance(param.value(), bool)
 		param = Parameter("Key", "False")
-		assert isinstance(param.value(), bool)
+		self.assertIsInstance(param.value(), bool)
 		param = Parameter("Key", "other")
-		assert isinstance(param.value(), basestring)
+		self.assertIsInstance(param.value(), basestring)
 
 	def testStringRepresentations(self):
 		# Test the string representations
 		# String value
 		param = Parameter("key", "value")
-		assert param.__str__() == '(key "value")'
+		self.assertEquals(param.__str__(), '(key "value")')
 		# Integer value
 		param.setValue(20)
-		assert param.__str__() == '(key 20)'
+		self.assertEquals(param.__str__(), '(key 20)')
 		# Float value
 		param.setValue(20.0)
-		assert param.__str__() == '(key 20.0)'
+		self.assertEquals(param.__str__(), '(key 20.0)')
 		# bool values
 		param.setValue(True)
-		assert param.__str__() == '(key "true")'
+		self.assertEquals(param.__str__(), '(key "true")')
 		param.setValue(False)
-		assert param.__str__() == '(key "false")'
+		self.assertEquals(param.__str__(), '(key "false")')
 
 	def testParameterFromString(self):
 		param = Parameter.parameterFromString("")
-		assert param is None
+		self.assertIsNone(param)
 
 		param = Parameter.parameterFromString("()")
-		assert param is None
+		self.assertIsNone(param)
 
 		param = Parameter.parameterFromString("( )")
-		assert param is None
+		self.assertIsNone(param)
 		
 		param = Parameter.parameterFromString("(a 1)")
-		assert param is not None
-		assert param.key() == "a"
-		assert param.value() == 1
+		self.assertIsNotNone(param)
+		self.assertEquals(param.key(), "a")
+		self.assertEquals(param.value(), 1)
 		
 		param = Parameter.parameterFromString("a (1)")
-		assert param is None
+		self.assertIsNone(param)
 
 		param = Parameter.parameterFromString(")(a 1")
-		assert param is None
+		self.assertIsNone(param)
 
 		param = Parameter.parameterFromString("(a b c")
-		assert param is None
+		self.assertIsNone(param)
 
 		param = Parameter.parameterFromString("(a b c)")
-		assert param is not None
-		assert param.key() == "a"
-		assert param.value() == "b c"
+		self.assertIsNotNone(param)
+		self.assertEquals(param.key(), "a")
+		self.assertEquals(param.value(), "b c")
 
 		param = Parameter.parameterFromString("(ab)")
-		assert param is None
+		self.assertIsNone(param)
--- tests/test_Transformation.py
+++ tests/test_Transformation.py
@@ -12,41 +12,38 @@ class TransformationTest(unittest.TestCase):
 		anotherValue = Parameter("key", 0)
 
 		parameters = Transformation()
-		assert len(parameters) == 0
+		self.assertEquals(len(parameters), 0)
 		# Test adding a non Parameter value
-		try:
-			parameters.append(nonvalidValue)
-			assert False
-		except Exception, e:
-			assert isinstance(e, TypeError)
+		self.assertRaises(TypeError, parameters.append, nonvalidValue)
 		
-		assert len(parameters) == 0
+		self.assertEquals(len(parameters), 0)
 		parameters.append(value)
-		assert len(parameters) == 1
-		assert parameters[0] == value
+		self.assertEquals(len(parameters), 1)
+		self.assertEquals(parameters[0], value)
 		parameters.append(otherValue)
-		assert len(parameters) == 2
-		assert parameters[1] == otherValue
+		self.assertEquals(len(parameters), 2)
+		self.assertEquals(parameters[1], otherValue)
 		parameters[0] = anotherValue
-		assert parameters[0] == anotherValue
+		self.assertEquals(parameters[0], anotherValue)
 		del parameters[1]
-		assert len(parameters) == 1
-		assert anotherValue in parameters
-		assert otherValue not in parameters
+		self.assertEquals(len(parameters), 1)
+		self.assertTrue(anotherValue in parameters)
+		self.assertTrue(otherValue not in parameters)
 
 	def testInputOutputMethods(self):
 		import os
 		parameters = Transformation()
 		path = os.path.dirname(os.path.abspath(__file__))
 		parameters.loadFromFile(unicode(path) + "/data/Sample.txt")
-		assert len(parameters) == 28
+		self.assertEquals(len(parameters), 28)
 		parameters.saveToFile(unicode(path) + "/SampleOutput.c")
 		otherParameters = Transformation()
 		otherParameters.loadFromFile(unicode(path) + "/SampleOutput.c")
-		assert len(parameters) == len(otherParameters)
-		assert Parameter("MovingInternalImagePixelType", "float") in parameters
-		assert parameters == otherParameters
+		self.assertEquals(len(parameters), len(otherParameters))
+		self.assertTrue(Parameter("MovingInternalImagePixelType", "float") in parameters)
+		self.assertEquals(parameters, otherParameters)
 
+		# Clean up sample output data
 		try:
 			import os
 			os.remove(unicode(path) + "/SampleOutput.c")
