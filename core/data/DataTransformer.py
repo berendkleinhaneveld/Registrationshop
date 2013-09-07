3e483c2dcfd89227d9a2c56578a6532439b8fca4
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2013-09-07 14:57:48 +0200

    Add a simple transform helper class.

---
+++ core/data/DataTransformer.py
@@ -0,0 +1,28 @@
+"""
+DataTransformer
+
+:Authors:
+	Berend Klein Haneveld
+"""
+
+from vtk import vtkImageReslice
+
+
+class DataTransformer(object):
+	"""DataTransformer is a class that can transform a given dataset"""
+	def __init__(self):
+		super(DataTransformer, self).__init__()
+
+	def TransformImageData(self, imageData, transform):
+		"""
+		:type imageData: vtkImageData
+		:type transform: vtkTransform
+		"""
+		reslicer = vtkImageReslice()
+		reslicer.SetInterpolationModeToCubic()
+		reslicer.SetAutoCropOutput(1)  # Not sure if this is what we want
+
+		reslicer.SetInputData(imageData)
+		reslicer.SetResliceTransform(transform.GetInverse())
+
+		return reslicer.GetOutput()
