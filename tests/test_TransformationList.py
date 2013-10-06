a1b304d41412cd4698a2a2e8b5d2a542e98a50cc
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2013-10-06 13:48:00 +0200

    Test for scaling matrix.

--- tests/test_TransformationList.py
+++ tests/test_TransformationList.py
@@ -75,3 +75,49 @@ class TransformationListTest(unittest.TestCase):
 		self.assertEquals(prevMatrix.GetElement(0, 3), 3)
 		self.assertEquals(prevMatrix.GetElement(1, 3), 3)
 		self.assertEquals(prevMatrix.GetElement(2, 3), 3)
+
+	def testScaling(self):
+		transform = vtkTransform()
+		transform.Scale(3.0, 3.0, 3.0)
+		transform.RotateX(45.0)
+		transform.RotateY(45.0)
+		transform.Translate(30, 15, 10)
+
+		self.transformList.append(transform)
+
+		scaleTransform = self.transformList.scalingTransform()
+		scaleMatrix = scaleTransform.GetMatrix()
+
+		self.assertAlmostEqual(scaleMatrix.GetElement(0, 0), 3, delta=0.001)
+		self.assertAlmostEqual(scaleMatrix.GetElement(1, 1), 3, delta=0.001)
+		self.assertAlmostEqual(scaleMatrix.GetElement(2, 2), 3, delta=0.001)
+		self.assertAlmostEqual(scaleMatrix.GetElement(3, 3), 1, delta=0.001)
+
+		transform = vtkTransform()
+		transform.RotateX(45.0)
+		transform.Scale(2.0, 1.0, 4.0)
+		transform.Translate(30, 15, 10)
+		transform.RotateY(45.0)
+
+		self.transformList.append(transform)
+
+		scaleTransform = self.transformList.scalingTransform()
+		scaleMatrix = scaleTransform.GetMatrix()
+		print scaleMatrix
+
+		self.assertAlmostEqual(scaleMatrix.GetElement(0, 0), 10.3891, delta=0.001)
+		self.assertAlmostEqual(scaleMatrix.GetElement(1, 1), 7.0356, delta=0.001)
+		self.assertAlmostEqual(scaleMatrix.GetElement(2, 2), 5.6183, delta=0.001)
+		self.assertAlmostEqual(scaleMatrix.GetElement(3, 3), 1.0000, delta=0.001)
+
+		comp = self.transformList.completeTransform()
+		print comp.GetMatrix()
+
+		self.assertFalse(True)
+
+		# ShearX: 3.99680288865e-15
+		# ShearY: -3.99680288865e-15
+		# ShearZ: 1.24344978758e-14
+		# ShearX: -55.6378246381
+		# ShearY: 1.63782463806
+		# ShearZ: 20.25
