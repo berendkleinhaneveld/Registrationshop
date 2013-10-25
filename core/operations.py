8442980b134cc5b648fa96239d2c3078490e8db8
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2013-10-25 14:33:53 +0200

    Add method for finding intersection of a line with triangle.

--- core/operations.py
+++ core/operations.py
@@ -9,6 +9,7 @@ Input is never adjusted.
 """
 import math
 import collections
+from vtk import vtkMath
 
 
 def Dot(u, v):
@@ -109,3 +110,53 @@ def ClosestPoints(p1, p2, q1, q2, clamp=False):
 		tc = min(tc, 1)
 
 	return Add(Multiply(u, sc), p1), Add(Multiply(v, tc), q1)
+
+
+def LineIntersectionWithTriangle(pointA, pointB, triangle):
+	"""
+	Solve linear system for intersection with plane
+	defined by plane through 3 points. If the first
+	value of outVector is between 0 and 1, it is on the
+	line between the 2 given points. If the other values
+	of outVector are also between 0 and 1 and they add up
+	to a max of 1, then the intersection is within the
+	specified triangle.
+	:rtype: bool, list(3)
+	"""
+	A = pointA
+	B = pointB
+	P0 = triangle[0]
+	P1 = triangle[1]
+	P2 = triangle[2]
+
+	matrix = [[0 for x in range(3)] for x in range(3)]
+	matrix[0][0] = A[0] - B[0]
+	matrix[1][0] = A[1] - B[1]
+	matrix[2][0] = A[2] - B[2]
+	matrix[0][1] = P1[0] - P0[0]
+	matrix[1][1] = P1[1] - P0[1]
+	matrix[2][1] = P1[2] - P0[2]
+	matrix[0][2] = P2[0] - P0[0]
+	matrix[1][2] = P2[1] - P0[1]
+	matrix[2][2] = P2[2] - P0[2]
+
+	inVector = [0 for x in range(3)]
+	inVector[0] = A[0] - P0[0]
+	inVector[1] = A[1] - P0[1]
+	inVector[2] = A[2] - P0[2]
+
+	outVector = [0 for x in range(3)]
+	vtkMath.LinearSolve3x3(matrix, inVector, outVector)
+
+	intersectsTriangle = False
+	if (outVector[0] >= 0 and outVector[0] <= 1
+		and outVector[1] >= 0 and outVector[1] <= 1
+		and outVector[2] >= 0 and outVector[2] <= 1
+		and outVector[1] + outVector[2] <= 1):
+		intersectsTriangle = True
+
+	# intersection = Ia + (Ib - Ia)*t
+	vec = Subtract(pointB, pointA)
+	vec = Multiply(vec, outVector[0])
+	intersection = Add(pointA, vec)
+	return intersectsTriangle, intersection
