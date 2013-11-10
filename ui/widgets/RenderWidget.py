15b70c6a80eb18647c47f3edc1a49aeece6727ea
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2013-11-10 21:00:54 +0100

    Make use of the vtkDrawing functions for drawing objects with the render widget.

--- ui/widgets/RenderWidget.py
+++ ui/widgets/RenderWidget.py
@@ -10,26 +10,14 @@ from vtk import vtkVolume
 from vtk import vtkInteractorStyleTrackballCamera
 from libvtkGPUMultiVolumeRenderPython import vtkOpenGLGPUVolumeRayCastMapper2 as vtkOpenGLGPUVolumeRayCastMapper
 from vtk import vtkTransform
-from vtk import vtkPolyDataMapper
-from vtk import vtkFollower
-from vtk import vtkAssembly
-from vtk import vtkLineSource
-from vtk import vtkVectorText
-from vtk import vtkDataSetMapper
-from vtk import vtkActor
 from vtk import vtkImagePlaneWidget
 from PySide.QtGui import QGridLayout
 from PySide.QtGui import QWidget
 from PySide.QtCore import Signal
 from PySide.QtCore import Slot
 from ui.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
-from core.operations import Multiply
-from core.operations import Subtract
-from core.operations import Add
-from core.operations import Length
-
-# gridNudges describes the possible values for indicator intervals for the grid
-gridNudges = [1, 5, 10, 50, 100, 500, 1000]
+from core.vtkDrawing import CreateBounds
+from core.vtkDrawing import CreateOrientationGrid
 
 
 class RenderWidget(QWidget):
@@ -193,7 +181,7 @@ class RenderWidget(QWidget):
 		These parts need to be updated when the user is transformed.
 		"""
 		bounds = self.imageData.GetBounds()
-		self.gridItems = CreateGridItems(bounds)
+		self.gridItems = CreateBounds(bounds)
 		for item in self.gridItems:
 			self.renderer.AddViewProp(item)
 
@@ -202,7 +190,7 @@ class RenderWidget(QWidget):
 		Create 3 axis that show the location / coords in 3D space.
 		"""
 		bounds = self.imageData.GetBounds()
-		self.orientationGridItems = CreateOrientationGridItems(bounds, self.renderer.GetActiveCamera())
+		self.orientationGridItems = CreateOrientationGrid(bounds, self.renderer.GetActiveCamera())
 		for item in self.orientationGridItems:
 			self.renderer.AddViewProp(item)
 
@@ -217,150 +205,3 @@ class RenderWidget(QWidget):
 		self.volume.SetUserTransform(transform)
 		for item in self.gridItems:
 			item.SetUserTransform(transform)
-
-
-def CreateOrientationGridItems(bounds, camera):
-	# TODO: instead of lines, spheres or other shapes could be used as nudges
-	boundX = bounds[1] * 1.2
-	boundY = bounds[3] * 1.2
-	boundZ = bounds[5] * 1.2
-
-	lineActorsX = []
-	lineActorsY = []
-	lineActorsZ = []
-
-	# Create the main axes
-	lineActorsX.append(CreateLine([0, 0, 0], [boundX, 0, 0]))
-	lineActorsY.append(CreateLine([0, 0, 0], [0, boundY, 0]))
-	lineActorsZ.append(CreateLine([0, 0, 0], [0, 0, boundZ]))
-
-	# Create the nudges on the X axis
-	subdivSize = boundX / 10
-	subdivSize = ClosestToMeasurement(subdivSize)
-	handleSize = subdivSize / 5.0
-
-	for index in range(1, int(boundX / subdivSize)):
-		lineActorsX.append(CreateLine([index * subdivSize, 0, 0], [index * subdivSize, handleSize, 0]))
-		lineActorsX.append(CreateLine([index * subdivSize, 0, 0], [index * subdivSize, 0, handleSize]))
-
-	textItemX = CreateTextItem("X", [boundX, 0, 0], [1, 1, 1], 0.5 * subdivSize, camera)
-
-	# Create the nudges on the Y axis
-	subdivSize = boundY / 10
-	subdivSize = ClosestToMeasurement(subdivSize)
-	handleSize = subdivSize / 5.0
-
-	for index in range(1, int(boundY / subdivSize)):
-		lineActorsY.append(CreateLine([0, index * subdivSize, 0], [handleSize, index * subdivSize, 0]))
-		lineActorsY.append(CreateLine([0, index * subdivSize, 0], [0, index * subdivSize, handleSize]))
-
-	textItemY = CreateTextItem("Y", [0, boundY, 0], [1, 1, 1], 0.5 * subdivSize, camera)
-
-	# Create the nudges on the Z axis
-	subdivSize = boundZ / 10
-	subdivSize = ClosestToMeasurement(subdivSize)
-	handleSize = subdivSize / 5.0
-
-	for index in range(1, int(boundZ / subdivSize)):
-		lineActorsZ.append(CreateLine([0, 0, index * subdivSize], [handleSize, 0, index * subdivSize]))
-		lineActorsZ.append(CreateLine([0, 0, index * subdivSize], [0, handleSize, index * subdivSize]))
-
-	textItemZ = CreateTextItem("Z", [0, 0, boundZ], [1, 1, 1], 0.5 * subdivSize, camera)
-
-	# Color the axis: R, G and B
-	for lineActor in lineActorsX:
-		lineActor.GetProperty().SetColor(1, 0, 0)
-	for lineActor in lineActorsY:
-		lineActor.GetProperty().SetColor(0, 1, 0)
-	for lineActor in lineActorsZ:
-		lineActor.GetProperty().SetColor(0, 0, 1)
-
-	# Add the lines into one big assembly
-	dataGrid = vtkAssembly()
-	for lineActor in (lineActorsX + lineActorsY + lineActorsZ):
-		dataGrid.AddPart(lineActor)
-
-	return [dataGrid, textItemX, textItemY, textItemZ]
-
-
-def CreateTextItem(text, position, color, scale, camera):
-	textSource = vtkVectorText()
-	textSource.SetText(text)
-
-	textMapper = vtkPolyDataMapper()
-	textMapper.SetInputConnection(textSource.GetOutputPort())
-
-	textFollower = vtkFollower()
-	textFollower.SetMapper(textMapper)
-	textFollower.SetCamera(camera)
-	textFollower.SetPosition(position)
-	textFollower.GetProperty().SetColor(color[0], color[1], color[2])
-	textFollower.SetScale(scale)
-
-	return textFollower
-
-
-def CreateGridItems(bounds):
-	boundX = bounds[1]
-	boundY = bounds[3]
-	boundZ = bounds[5]
-
-	linePartLength = 0.2
-
-	lineActors = []
-	lineActors += CreateLineBeginAndEnd([0, 0, 0], [boundX, 0, 0], linePartLength)
-	lineActors += CreateLineBeginAndEnd([0, 0, 0], [0, boundY, 0], linePartLength)
-	lineActors += CreateLineBeginAndEnd([0, 0, 0], [0, 0, boundZ], linePartLength)
-
-	lineActors += CreateLineBeginAndEnd([boundX, boundY, boundZ], [boundX, boundY, 0], linePartLength)
-	lineActors += CreateLineBeginAndEnd([boundX, boundY, boundZ], [0, boundY, boundZ], linePartLength)
-	lineActors += CreateLineBeginAndEnd([boundX, boundY, boundZ], [boundX, 0, boundZ], linePartLength)
-
-	lineActors += CreateLineBeginAndEnd([boundX, 0, 0], [boundX, 0, boundZ], linePartLength)
-	lineActors += CreateLineBeginAndEnd([boundX, 0, 0], [boundX, boundY, 0], linePartLength)
-	lineActors += CreateLineBeginAndEnd([0, boundY, 0], [0, boundY, boundZ], linePartLength)
-	lineActors += CreateLineBeginAndEnd([0, boundY, 0], [boundX, boundY, 0], linePartLength)
-	lineActors += CreateLineBeginAndEnd([0, 0, boundZ], [0, boundY, boundZ], linePartLength)
-	lineActors += CreateLineBeginAndEnd([0, 0, boundZ], [boundX, 0, boundZ], linePartLength)
-
-	dataGrid = vtkAssembly()
-	for lineActor in lineActors:
-		dataGrid.AddPart(lineActor)
-
-	return [dataGrid]
-
-
-def ClosestToMeasurement(number):
-	# Calculate diff
-	diff = map(lambda x: abs(x - number), gridNudges)
-	index = diff.index(min(diff))
-	return gridNudges[index]
-
-
-def CreateLine(p1, p2):
-	lineSource = vtkLineSource()
-	lineSource.SetPoint1(p1[0], p1[1], p1[2])
-	lineSource.SetPoint2(p2[0], p2[1], p2[2])
-
-	lineMapper = vtkDataSetMapper()
-	lineMapper.SetInputConnection(lineSource.GetOutputPort())
-
-	lineActor = vtkActor()
-	lineActor.SetMapper(lineMapper)
-	return lineActor
-
-
-def CreateLineBeginAndEnd(p1, p2, length):
-	"""
-	Length is value between 0 and 0.5 to specify how long
-	each begin and end part is compared to the complete line.
-	"""
-	point1 = p1
-	point2 = Add(p1, Multiply(Subtract(p2, p1), length))
-	point3 = p2
-	point4 = Add(p2, Multiply(Subtract(p1, p2), length))
-
-	line1 = CreateLine(point1, point2)
-	line2 = CreateLine(point3, point4)
-	
-	return [line1, line2]
