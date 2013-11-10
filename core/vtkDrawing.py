dc17eca0c44648aa94eee07d1525b26a45aae0d8
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2013-11-10 20:59:27 +0100

    Add convenience methods for creating vtk objects.
    Stuff like lines, spheres, circles and bounds for volumes. Anything
    useful that can be displayed in a vtkRenderer.

---
+++ core/vtkDrawing.py
@@ -0,0 +1,259 @@
+"""
+vtkDrawing
+
+Convenience methods for creating simple vtk objects
+that can be used in renderers.
+Call one of the methods with some custom parameters
+and out comes a vtkActor that can be given to a
+renderer with AddViewProp().
+
+:Authors:
+	Berend Klein Haneveld
+"""
+
+from vtk import vtkActor
+from vtk import vtkVectorText
+from vtk import vtkLineSource
+from vtk import vtkSphereSource
+from vtk import vtkRegularPolygonSource
+from vtk import vtkDataSetMapper
+from vtk import vtkPolyDataMapper
+from vtk import vtkFollower
+from vtk import vtkAssembly
+from core.operations import Add
+from core.operations import Subtract
+from core.operations import Multiply
+
+
+def ColorActor(actor, color, opacity=None):
+	"""
+	Give the actor a custom color is one is provided.
+	"""
+	if not color:
+		return
+	
+	actor.GetProperty().SetColor(color[0], color[1], color[2])
+	if opacity:
+		actor.GetProperty().SetOpacity(opacity)
+
+
+def CreateLine(p1, p2, color=None):
+	"""
+	Creates a line between p1 and p2.
+	"""
+	lineSource = vtkLineSource()
+	lineSource.SetPoint1(p1[0], p1[1], p1[2])
+	lineSource.SetPoint2(p2[0], p2[1], p2[2])
+
+	lineMapper = vtkDataSetMapper()
+	lineMapper.SetInputConnection(lineSource.GetOutputPort())
+
+	lineActor = vtkActor()
+	lineActor.SetMapper(lineMapper)
+
+	# Give the actor a custom color
+	ColorActor(lineActor, color)
+
+	return lineActor
+
+
+def CreateLineBeginAndEnd(p1, p2, length, color=None):
+	"""
+	Length is value between 0 and 0.5 to specify how long
+	each begin and end part is compared to the complete line.
+	:rtype: list of line actors
+	"""
+	point1 = p1
+	point2 = Add(p1, Multiply(Subtract(p2, p1), length))
+	point3 = p2
+	point4 = Add(p2, Multiply(Subtract(p1, p2), length))
+
+	line1 = CreateLine(point1, point2, color)
+	line2 = CreateLine(point3, point4, color)
+	
+	return [line1, line2]
+
+
+def CreateSphere(radius, color=None):
+	sphereSource = vtkSphereSource()
+	sphereSource.SetRadius(radius)
+	sphereSource.SetThetaResolution(18)
+	sphereSource.SetPhiResolution(18)
+
+	sphereMapper = vtkPolyDataMapper()
+	sphereMapper.SetInputConnection(sphereSource.GetOutputPort())
+
+	sphereActor = vtkActor()
+	sphereActor.PickableOff()
+	sphereActor.SetMapper(sphereMapper)
+
+	# Give the actor a custom color
+	ColorActor(sphereActor, color)
+
+	# Also give the sphere object the convenience methods of
+	# SetCenter() and GetCenter() that misses from the vtkActor
+	# class but is present in the vtkSphereSource class
+	def setCenter(x, y, z):
+		sphereSource.SetCenter(x, y, z)
+
+	def getCenter():
+		return sphereSource.GetCenter()
+
+	setattr(sphereActor, "SetCenter", setCenter)
+	setattr(sphereActor, "GetCenter", getCenter)
+
+	return sphereActor
+
+
+def CreateTextItem(text, scale, camera, color=None):
+	textSource = vtkVectorText()
+	textSource.SetText(text)
+
+	textMapper = vtkPolyDataMapper()
+	textMapper.SetInputConnection(textSource.GetOutputPort())
+
+	textFollower = vtkFollower()
+	textFollower.SetMapper(textMapper)
+	textFollower.SetCamera(camera)
+	textFollower.SetScale(scale)
+
+	# Give the actor a custom color
+	ColorActor(textFollower, color)
+
+	return textFollower
+
+
+def CreateCircle(radius):
+	circleSource = vtkRegularPolygonSource()
+	circleSource.SetNumberOfSides(30)
+	circleSource.SetRadius(radius)
+	circleSource.SetGeneratePolygon(False)
+
+	circleMapper = vtkPolyDataMapper()
+	circleMapper.SetInputConnection(circleSource.GetOutputPort())
+
+	circle = vtkActor()
+	circle.PickableOff()
+	circle.SetMapper(circleMapper)
+	circle.GetProperty().SetColor(1.0, 0.5, 0.5)
+
+	return circle
+
+
+def CreateBounds(bounds):
+	"""
+	Creates a boundary object to display around a volume.
+	:rtype: list of actors
+	"""
+	boundX = bounds[1]
+	boundY = bounds[3]
+	boundZ = bounds[5]
+
+	linePartLength = 0.2
+
+	lineActors = []
+	lineActors += CreateLineBeginAndEnd([0, 0, 0], [boundX, 0, 0], linePartLength)
+	lineActors += CreateLineBeginAndEnd([0, 0, 0], [0, boundY, 0], linePartLength)
+	lineActors += CreateLineBeginAndEnd([0, 0, 0], [0, 0, boundZ], linePartLength)
+
+	ColorActor(lineActors[0], [1, 0, 0])
+	ColorActor(lineActors[2], [0, 1, 0])
+	ColorActor(lineActors[4], [0, 0, 1])
+
+	lineActors += CreateLineBeginAndEnd([boundX, boundY, boundZ], [boundX, boundY, 0], linePartLength)
+	lineActors += CreateLineBeginAndEnd([boundX, boundY, boundZ], [0, boundY, boundZ], linePartLength)
+	lineActors += CreateLineBeginAndEnd([boundX, boundY, boundZ], [boundX, 0, boundZ], linePartLength)
+
+	lineActors += CreateLineBeginAndEnd([boundX, 0, 0], [boundX, 0, boundZ], linePartLength)
+	lineActors += CreateLineBeginAndEnd([boundX, 0, 0], [boundX, boundY, 0], linePartLength)
+	lineActors += CreateLineBeginAndEnd([0, boundY, 0], [0, boundY, boundZ], linePartLength)
+	lineActors += CreateLineBeginAndEnd([0, boundY, 0], [boundX, boundY, 0], linePartLength)
+	lineActors += CreateLineBeginAndEnd([0, 0, boundZ], [0, boundY, boundZ], linePartLength)
+	lineActors += CreateLineBeginAndEnd([0, 0, boundZ], [boundX, 0, boundZ], linePartLength)
+
+	mean = reduce(lambda x, y: x + y, bounds) / 3.0
+	sphereActor = CreateSphere(mean / 25.0)
+	sphereActor.SetPosition(0, 0, 0)
+
+	dataGrid = vtkAssembly()
+	for lineActor in lineActors:
+		dataGrid.AddPart(lineActor)
+
+	return [dataGrid, sphereActor]
+
+
+def CreateOrientationGrid(bounds, camera):
+	# TODO: instead of lines, spheres or other shapes could be used as nudges
+	boundX = bounds[1] * 1.2
+	boundY = bounds[3] * 1.2
+	boundZ = bounds[5] * 1.2
+
+	lineActorsX = []
+	lineActorsY = []
+	lineActorsZ = []
+
+	# Create the main axes
+	lineActorsX.append(CreateLine([0, 0, 0], [boundX, 0, 0]))
+	lineActorsY.append(CreateLine([0, 0, 0], [0, boundY, 0]))
+	lineActorsZ.append(CreateLine([0, 0, 0], [0, 0, boundZ]))
+
+	# Create the nudges on the X axis
+	subdivSize = boundX / 10
+	subdivSize = ClosestToMeasurement(subdivSize)
+	handleSize = subdivSize / 5.0
+
+	for index in range(1, int(boundX / subdivSize)):
+		lineActorsX.append(CreateLine([index * subdivSize, 0, 0], [index * subdivSize, handleSize, 0]))
+		lineActorsX.append(CreateLine([index * subdivSize, 0, 0], [index * subdivSize, 0, handleSize]))
+
+	textItemX = CreateTextItem("X", 0.5 * subdivSize, camera)
+	textItemX.SetPosition([boundX, 0, 0])
+
+	# Create the nudges on the Y axis
+	subdivSize = boundY / 10
+	subdivSize = ClosestToMeasurement(subdivSize)
+	handleSize = subdivSize / 5.0
+
+	for index in range(1, int(boundY / subdivSize)):
+		lineActorsY.append(CreateLine([0, index * subdivSize, 0], [handleSize, index * subdivSize, 0]))
+		lineActorsY.append(CreateLine([0, index * subdivSize, 0], [0, index * subdivSize, handleSize]))
+
+	textItemY = CreateTextItem("Y", 0.5 * subdivSize, camera)
+	textItemY.SetPosition([0, boundY, 0])
+
+	# Create the nudges on the Z axis
+	subdivSize = boundZ / 10
+	subdivSize = ClosestToMeasurement(subdivSize)
+	handleSize = subdivSize / 5.0
+
+	for index in range(1, int(boundZ / subdivSize)):
+		lineActorsZ.append(CreateLine([0, 0, index * subdivSize], [handleSize, 0, index * subdivSize]))
+		lineActorsZ.append(CreateLine([0, 0, index * subdivSize], [0, handleSize, index * subdivSize]))
+
+	textItemZ = CreateTextItem("Z", 0.5 * subdivSize, camera)
+	textItemZ.SetPosition([0, 0, boundZ])
+
+	# Color the axis: R, G and B
+	for lineActor in lineActorsX:
+		ColorActor(lineActor, [1, 0, 0])
+	for lineActor in lineActorsY:
+		ColorActor(lineActor, [0, 1, 0])
+	for lineActor in lineActorsZ:
+		ColorActor(lineActor, [0, 0, 1])
+
+	# Add the lines into one big assembly
+	dataGrid = vtkAssembly()
+	for lineActor in (lineActorsX + lineActorsY + lineActorsZ):
+		dataGrid.AddPart(lineActor)
+
+	return [dataGrid, textItemX, textItemY, textItemZ]
+
+
+def ClosestToMeasurement(number):
+	# gridNudges describes the possible values for indicator intervals for the grid
+	gridNudges = [1, 5, 10, 50, 100, 500, 1000]
+
+	# Calculate diff
+	diff = map(lambda x: abs(x - number), gridNudges)
+	index = diff.index(min(diff))
+	return gridNudges[index]
