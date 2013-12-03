6482d69cac6a391d29532b7ad1326991c7970eb6
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2013-12-03 18:00:58 +0100

    Provide a means to use a format for the transformed data.
    Passing in an info dataset will use the spacing and extents of that
    dataset for producing the transformed dataset.

--- core/data/DataTransformer.py
+++ core/data/DataTransformer.py
@@ -13,14 +13,20 @@ class DataTransformer(object):
 	def __init__(self):
 		super(DataTransformer, self).__init__()
 
-	def TransformImageData(self, imageData, transform):
+	def TransformImageData(self, imageData, transform, infoData=None):
 		"""
+		The properties of infoData (optional) are used for generating the new dataset.
+		The default is to use the properties of the given image data.
+
 		:type imageData: vtkImageData
 		:type transform: vtkTransform
+		:type infoData: vtkImageData
 		"""
 		range = imageData.GetScalarRange()
 		reslicer = vtkImageReslice()
 		reslicer.SetInterpolationModeToCubic()
+		if infoData:
+			reslicer.SetInformationInput(infoData)
 		reslicer.SetBackgroundLevel(range[0])
 		reslicer.AutoCropOutputOff()
 		reslicer.SetInputData(imageData)
